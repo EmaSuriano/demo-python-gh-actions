f77dee5ff3c7ba4ec91e2182d81dc35920b1b328
Emanuel Suriano <emanuel.suriano@revolut.com>
2025-01-15 15:15:50 +0100

    remove unused deps

--- train.py
+++ train.py
@@ -5,8 +5,6 @@ from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import StandardScaler
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.metrics import classification_report, confusion_matrix
-import matplotlib.pyplot as plt
-import seaborn as sns
 import joblib  # Add this import
 
 
